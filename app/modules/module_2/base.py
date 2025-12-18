639416c2c40e97ed589ee5e50e3a5b12befb4f9f
aleynakayalik <aleynakayalik312@gmail.com>
2025-12-18 23:43:00 +0300

    Module 2: PaymentMethod base ve 3 ödeme yöntemi eklendi

--- app/modules/module_2/base.py
+++ app/modules/module_2/base.py
@@ -1,76 +1,128 @@
-# app/modules/module_2/base.py
-#from abc import ABC, abstractmethod
-
-class BaseClass2(ABC):
-    def __init__(self, parameter: str):
-        self.base2Attribute = parameter
-
-    @abstractmethod
-    def method2(self) -> None:
-        # model tanimii
-        pass
-
-
-
+from __future__ import annotations
 from abc import ABC, abstractmethod
 
-class OdemeYontemi(ABC):
-    def __init__(self, kisi: str ,bakiye: float , para_birimi="TL"):  #attribute
-        self.kisi = kisi 
-        self.bakiye = bakiye       
-        self.para_birimi = para_birimi
+class PaymentMethod(ABC):
+    def __init__(self, owner: str, currency: str, balance: float | None = None, limit: float| None = None):
+        self.__owner = None
+        self.__currency = None
+        self.__balance = None
+        self.__limit = None
 
+        self.set_owner(owner)
+        self.set_currency(currency)
+        self.set_balance(balance)
+        self.set_limit(limit)
+    #ödeme yönteminin sahibini döndürür -getter-
+    def get_owner(self) -> str:
+        return self.__owner
+    #ödeme yönteminin sahibini ayarlar -setter-
+    def set_owner(self, owner: str) ->  None:
+        if not isinstance(owner, str):
+            raise ValueError("owner bir metin olmalıdır.")
+        owner = owner.strip()
+        if owner == "":
+            raise ValueError("owner boş olamaz.")
+        if len(owner) < 2:
+            raise ValueError("owner en az 2 karakter olmalıdır.")
+        self.__owner = owner
+    #para birimini döndürür -getter-
+    def get_currency(self) -> str:
+        return self.__currency
+    #para birimini ayarar -setter-
+    def set_currency(self, currency: str) -> None:
+        if not isinstance(currency, str):
+            raise ValueError("currency metin olmalıdır.")
+        currency = currency.strip()
+        if currency == "":
+            raise ValueError("currency boş olamaz.")
+        currency = currency.upper()
+        if currency not in ["TRY","USD", "EUR"]:
+            raise ValueError("Geçersiz para birimi.")
+        self.__currency = currency
+    #bakiye'yi döndürür -getter-
+    def get_balance(self) -> float | None:
+        return self.__balance
+    #bakiye değerini ayarlar -setter-
+    def set_balance(self, balance: float | None) -> None:
+        if balance is None:
+            self.__balance = None 
+            return
+        if not isinstance(balance, (int,float)):
+            raise ValueError("balance sayı olmalıdır.")
+        if balance < 0:
+            raise ValueError("balance negatif olamaz.")
+        
+        self.__balance = float(balance)
+
+    #limit değerini döndürür -getter-
+    def get_limit(self) -> float | None:
+        return self.__limit
+    #limit değerini ayarlar -setter-
+    def set_limit(self, limit: float | None) -> None:
+        if limit is None:
+            self.__limit = None
+            return
+        if not isinstance(limit, (int, float)) or limit < 0:
+                raise ValueError("limit negatif olamaz.")
+        self.__limit = float(limit)
+    
+    #öödeme yöntemiyle en fazla ne kadar ödenebilir
+    def kullanilabilir_tutar(self) -> float:
+        if self.__balance is not None:
+            return self.__balance
+        if self.__limit is not None:
+            return self.__limit
+        return 0.0
+    
+    #ödeme yapılmadan önce kontrol izin adımı
     @abstractmethod
-    def yetkilendir(self, tutar: float) -> bool: #Ödeme yapılabilir mi kontrol eder
+    def authorize(self, amount: float) -> bool:
+        pass
+    
+    #ödeme işlemi ve sonucu
+    @abstractmethod
+    def odeme_yap(self, amount: float) -> bool:
+        pass
+    
+    #yapılan ödemenin iadesi gerçekleştirir
+    @abstractmethod
+    def iade_yap(self, amount: float) -> bool:
         pass
 
-    @abstractmethod
-    def odeme_yap(self, tutar: float) -> bool:  #Ödeme işlemini gerçekleştirir
-        pass
-    #her alt sınıfta farklı olduğu için
-    @abstractmethod
-    def odeme_tipi(self) -> str:
-        pass
+    #girilen tutar geçerli mi kontrol eder
+    @staticmethod
+    def tutar_gecerli_mi(amount: float) -> bool:
+        if not isinstance(amount, (int,float)):
+            return False
+        if amount <= 0:
+            return False
+        return True
+    
+    #para birimi geçerli mi değil mi kontrol eder
+    @staticmethod
+    def para_birimi_gecerli_mi(currency: str) -> bool:
+        if not isinstance(currency, str):
+            return False
+        currency = currency.strip().upper()
+
+        if currency not in ["TRY", "USD","EUR"]:
+            return False
+        return True
+    #varsayılan para birimini döndürür
+    @classmethod
+    def varsayilan_currency(cls) -> str:
+        return "TRY"
+    
+    def get_info(self) -> str: #kısa bilgi döndürür
+        trip = self.__class__.__name__
+        tutar = self.kullanilabilir_tutar()
+        return f"{trip} | Kişi: {self.__owner} | Para Birimi: {self.__currency} | Kullanılabilir Tutar: {tutar}"
+    
     
 
-    #getter metotlar
-    def get_bakiye(self):
-        return self.bakiye
-    
-    def get_kisi(self):
-        return self.kisi
-    
-    def get_para_birimi(self):
-        return self.para_birimi
-    
-    #ortak metotlar
 
-    def yeterli_bakiye_mi(self,tutar: float) -> bool:   #yeterli bakiye var mı kontrolu
-        return self.bakiye >= tutar
 
-    def bakiye_dus(self, tutar: float) -> None:
-        self.bakiye -= tutar
-
-    def bakiye_arttir(self, tutar: float) -> None:
-        self.bakiye += tutar
-
-    def ozet_bilgi(self) -> str:
-        return (
-            f"Kişi : {self.kisi}" |
-            f"Bakiye : {self.bakiye} {self.para_birimi}" |
-            f"Ödeme Yöntemi : {self.__class__.__name__}" |
-            f"Ödeme Tipi : {self.odeme_tipi()}" #polimorfizm
-        )
-    #tüm ödeme yöntemleri için ortak olduğu için
-    def islem_kaydi(self, tutar: float, sonuc: bool, aciklama: str = "") -> dict:
-        return {
-            "kisi": self.kisi,
-            "odeme_tipi": self.odeme_tipi(), #polimorfizm Kredi Kartı şeklinde çıktı verir
-            "sinif_adı": self.__class__.__name__, #teknik çıktı olur KrediKartiOdeme gibi
-            "tutar": tutar,
-            "para_birimi": self.para_birimi,
-            "sonuc": sonuc,
-            "aciklama": aciklama
-        }
     
-    
+
+        
+        
--- app/modules/module_2/implementations.py
+++ app/modules/module_2/implementations.py
@@ -1,148 +1,314 @@
-# app/modules/module_2/implementations.py
-from app.modules.module_2.base import BaseClass2
+from app.modules.module_2.base import PaymentMethod
 
-class Base2SubClass1(BaseClass2):
-    def method2(self):
-        print(f"Sinif Attribute: {self.base2Attribute}")
-
-class Base2SubClass2(BaseClass2):
-    def method2(self):
-        print(f"Sinif Attribute: {self.base2Attribute}")
-
-
-
-from app.modules.module_2.base import OdemeYontemi 
-
-class KrediKartiOdeme(OdemeYontemi): ##subclass
-   def __init__(self, kisi: str, bakiye: float, para_birimi: str, kart_numarası: str = "" ,kart_ismi: str = "",cvv: int =0):
-     super().__init__(kisi, bakiye, para_birimi)
-     self.kart_numarasi = kart_numarasi
-     self.kart_ismi = kart_ismi
-     self.cvv = cvv #attribute
-
-   def odeme_tipi(self) -> str:
-      return "Kredi Kartı"
-   
-   def yetkilendir(self, tutar: float) -> bool:
-    return self.yeterli_bakiye_mi(tutar)
-   
-   def odeme_yap(self, tutar: float) -> bool:
-      if not self.yetkilendir(tutar):
-         #işlem kaydı oluşturma
-         kayit = self.islem_kaydi(tutar = tutar , sonuc = False , aciklama = "Kredi Kartı ödemesi reddedildi : YETERSİZ BAKİYE")
-         print("[Kredi Kartı] Ödeme başarısız :(")
-         print(kayit)
-         return False
+class CashPayment(PaymentMethod):#balance var limit yok
+   def __init__(self, owner: str, balance: float, currency: str = None):
+      if currency is None:
+         currency = self.varsayilan_currency()  # currency verilmezse varsayılan kullanılır
+      super().__init(owner = owner, balance = balance, currency = currency,limit = None)
       
-      self.bakiye_dus(tutar)
-      #işlem kaydı oluşturma
-      kayit = self.islem_kaydi(tutar = tutar, sonuc = True, aciklama = "[Kredi Kartı] ile yapılan İŞLEM BAŞARILI :)")
-      
-      print(
-            f"""
-        KREDİ KARTI ÖDEMESİ
-----------------------------------
-Kart No   : {self.kart_numarasi}
-Kart Adı  : {self.kart_ismi}
-Tutar     : {tutar} {self.para_birimi}
-Kalan     : {self.bakiye} {self.para_birimi}
-----------------------------------
-"""
-        )
-      print(self.ozet_bilgi())
-      print("İşlem Kaydı : ",kayit)
-      return True
-   
-    
-
-class NakitOdeme(OdemeYontemi):
-   def __init__(self, kisi: str, bakiye: float, para_birimi: str = "TL"):
-      super().__init__(kisi, bakiye, para_birimi)
-      self.fis_no = 0    
-      self.gunluk_islem_sayisi = 0
-
-   def odeme_tipi(self) -> str:
-      return "Nakit"
-
-   #kasadaki nakit parayı kontrol ediyor
-   def yetkilendir(self, tutar: float) -> bool:   
-      return self.yeterli_bakiye_mi(tutar)
-   
-   def odeme_yap(self, tutar: float) -> bool:
-      if not self.yetkilendir(tutar):
-         #işlem kaydı oluşturma
-         kayit = self.islem_kaydi(tutar = tutar, sonuc = False, aciklama = "Nakit Ödeme reddedildi : YTERSİZ BAKİYE")
-         print("[Nakit] ödeme başarısız")
-         print(kayit)
-         return False
-      
-      self.bakiye_dus(tutar)
-      self.fis_no += 1
-      self.gunluk_islem_sayisi += 1
-      #işlem kaydı oluşturma
-      kayit = self.islem_kaydi(tutar = tutar, sonuc = True, aciklama = "Nakit ödeme başarılı")
-      
-      print(
-            f"""
-           NAKİT ÖDEME
-----------------------------------
-Fiş No              : {self.fis_no}
-Günlük İşlem Sayısı : {self.gunluk_islem_sayisi}
-Ödenen Tutar        : {tutar} {self.para_birimi}
-Kalan Bakiye        : {self.bakiye} {self.para_birimi}
-----------------------------------
-"""        )
-      print(self.ozet_bilgi())
-      print("işlem Kaydı:",kayit)
-      return True
-   
-   
-class OnlineCuzdanOdeme(OdemeYontemi):
-   def __init__(self, kisi:str, bakiye: float, para_birimi: str = "TL", hesap_id: str = ""):
-      super().__init__(kisi, bakiye, para_birimi)
-      self.hesap_id = hesap_id
-      self.basarisiz_deneme_sayisi = 0
-      self.toplam_islem_sayisi = 0
-
-   def odeme_tipi(self) -> str:
-      return "Online Cüzdan"
-   #kasadaki nakit parayı kontrol ediyor
-   def yetkilendir(self, tutar: float) -> bool:
-      return self.yeterli_bakiye_mi(tutar)
-   
-   def odeme_yap(self, tutar: float) -> bool:
-      if not self.yetkilendir(tutar):
-            self.basarisiz_deneme_sayisi += 1
-            #işlem kaydı oluşturma
-            kayit = self.islem_kaydi(tutar=tutar, sonuc=False,aciklama="Online cüzdan ödeme reddedildi: yetersiz bakiye")
-            
-            print("[Online Cüzdan] Ödeme başarısız:(")
-            print("Başarısız deneme sayısı:", self.basarisiz_deneme_sayisi)
-            print(kayit)
+      #Ödeme yapılmadan önce yeterli bakiye var mı kontrol
+      def authorize(self, amount: float) -> bool: #Abstract method override
+         if not self.tutar_gecerli_mi(amount): #girilen tutar geçerli mi diye bakıyoruz
             return False
+         bakiye = self.get_balance()
+         if bakiye is None:
+            return False
+         #Bakiyemiz, ödemek istediğimiz tutardan büyük ya da eşitse ödeme yapılabilir
+         if bakiye >= amount:
+            return True
+         return False
       
-      self.bakiye_dus(tutar)
-      self.toplam_islem_sayisi += 1     
-      #işlem kaydı oluşturma
-      kayit = self.islem_kaydi(tutar = tutar, sonuc = True,aciklama = "Online cüzdan ödeme başarılı:)") 
+      #nakit ödeme işlemi
+      def odeme_yap(self, amount: float) -> bool: #Abstract method override
+         if not self.authorize(amount):
+            return False
+         
+         yeni_bakiye = self.get_balance() - amount
+         self.set_balance(yeni_bakiye)
+         return True
       
-      print(
-            f"""
-        ONLINE CÜZDAN ÖDEMESİ
-----------------------------------
-Hesap ID            : {self.hesap_id}
-Ödenen Tutar        : {tutar} {self.para_birimi}
-Toplam İşlem Sayısı : {self.toplam_islem_sayisi}
-Kalan Bakiye        : {self.bakiye} {self.para_birimi}
-----------------------------------
-""")
-      print(self.ozet_bilgi())
-      print("İşlem Kaydı:", kayit)
-      return True
+      #yapılan ödemenin iadesi
+      def iade_yap(self, amount: float) -> bool: #Abstract method override
+         if not self.tutar_gecerli_mi(amount):
+            return False
+         yeni_bakiye = (self.get_balance() or 0) + amountself.set_balance(yeni_bakiye)
+         return True
+      
+      #ödeme yöntemi nakit mi kontrolü
+      @staticmethod
+      def nakit_mi() -> bool:
+         return True
+      
+      #sınıf hakkında kısa bilgi
+      @classmethod
+      def odeme_tipi(cls) -> str:
+         return "Nakit Ödeme"
+      
+      
+class CreditCardPayment(PaymentMethod): #limit kullanılır balance kullanılmaz
    
-   
+   def __init__(self,owner: str,card_no: str,card_holder_name: str,expiry_date: str,cvv: str,limit: float,currency: str = None):
+      #para birimi verilmezse varsayılan kullanılır
+      if currency is None:
+          currency = self.varsayilan_currency()
+         
+      super().__ini__(owner = owner, currency = currency, balance = None, limit = limit)
+       
+      #kredi kartına özeller
+      self.__card_no = None
+      self.__card_holder_name = None
+      self.__expiry_date = None
+      self.__cvv = None
+
+      #setterlar ile doğrulayarak atama
+      self.set_card_no(card_no)
+      self.set_card_holder_name(card_holder_name)
+      self.set_expiry_date(expiry_date)
+      self.set_cvv(cvv)
+
+      #kart numarası
+      def get_card_no(self) -> str:
+         return self.__card_no
+      def set_card_no(self, card_no: str) -> None:
+         if not isinstance(card_no,str):
+            raise ValueError("Kart numarası metin olmalıdır.")
+         
+         card_no = card_no.strip().replace(" ","")
+         
+         if not card_no.isdigit():
+            raise ValueError("Kartt numarası sadece rakam içermeli.")
+         if len(card_no) < 12 and len(card_no) > 19:
+            raise ValueError("Kart numarası 12-19 haneli olmalıdır.")
+         
+         self.__card_no = card_no
+
+      #kart sahini
+      def get_card_holder_name(self) -> str:
+         return self.__card_holder_name
+      def set_card_holder_name(self, name: str) -> None:
+         if not isinstance(name, str):
+            raise ValueError("Kart sahibinin adı metin olmalıdır.")
+         
+         name= name.stript()
+         if name =="":
+            raise ValueError("Kart sahibinin adı boş olamaz.")
+         if len(name) < 3 :
+            raise ValueError("Kart sahibinin adı çok kısa.")
+         self.__card_holder_name = name
+
+      #son kullanma tarihi cvv
+      def get_expiry_date(self) -> str:
+         return self.__expiry_date
+      def set_expiry_date(self, expiry_date: str) -> None:
+         if not isinstance(expiry_date, str):
+            raise ValueError("Son kullanma tarihi metin olmalıdır.")
+         
+         expiry_date = expiry_date.stript()
+
+         #MM/YY
+         if len(expiry_date) != 5 or expiry_date[2] != "/":
+            raise ValueError("Son kullanma tarihi MM/YY şeklinde olmalıdır.")
+         
+         ay, yil = expiry_date.split("/")
+
+         if not ay.isdigit() or not yil.isdigit():
+            raise ValueError("Son kullanma tarihi sayısal olmalıdır.")
+         if not (1 <= int(ay) <= 12):
+            raise ValueError("Ay 1 ile 12 arasında olmalıdır.")
+         
+         self.__expiry_dare = expiry_date
+
+      #cvv
+      def get_cvv(self) -> str:
+        return self.__cvv
+
+      def set_cvv(self, cvv: str) -> None:
+         if not isinstance(cvv, str):
+            raise ValueError("CVV metin olmalıdır.")
+
+         cvv = cvv.strip()
+
+         if not cvv.isdigit():
+            raise ValueError("CVV sadece rakam olmalıdır.")
+         if len(cvv) not in (3, 4):
+            raise ValueError("CVV 3 veya 4 haneli olmalıdır.")
+
+         self.__cvv = cvv
+
+      #Kart numarasını ekranda gizli göstermek için
+      def masked_card_no(self) -> str:
+         return "*" * (len(self.__card_no) - 4) + self.__card_no[-4:]
+      
+      #ödeme işlemleri
+      def authorize(self, amount: float) -> bool: #Tutar geçerli mi
+         if not self.tutar_gecerli_mi(amount):
+            return False
+
+         limit = self.get_limit()
+         if limit is None:
+            return False
+
+         if limit >= amount:
+            return True
+
+         return False
+
+      def odeme_yap(self, amount: float) -> bool:
+         # Önce kontrol ediyor
+         if not self.authorize(amount):
+            return False
+
+         # Limitten düş
+         yeni_limit = self.get_limit() - amount
+         self.set_limit(yeni_limit)
+
+         return True
+
+      def iade_yap(self, amount: float) -> bool:
+         if not self.tutar_gecerli_mi(amount):
+            return False
+
+         yeni_limit = (self.get_limit() or 0) + amount
+         self.set_limit(yeni_limit)
+
+         return True
+
+      @staticmethod
+      def kart_tipi() -> str:
+          return "Kredi Kartı"
+
+      @classmethod
+      def odeme_tipi(cls) -> str:
+          return "Credit Card"
          
 
+      def get_info(self) -> str:
+          return (
+            f"{self.__class__.__name__} | "
+            f"Kişi: {self.get_owner()} | "
+            f"Kart Sahibi: {self.__card_holder_name} | "
+            f"Kart: {self.masked_card_no()} | "
+            f"SKT: {self.__expiry_date} | "
+            f"Limit: {self.kullanilabilir_tutar():.2f}"
+        )
+
+
+class OnlineWalletPayment(PaymentMethod): #balance ile çalışır
+
+   def __init__(self, owner: str, wallet_id: str, balance: float, aktif_mi: bool = True, currency: str = None):
+      if currency is None:
+         currency = self.varsayilan_currency()
+
+      super().__init__(owner=owner, currency=currency, balance=balance, limit=None)
+
+      self.__wallet_id = None
+      self.__aktif_mi = None
+
+      self.set_wallet_id(wallet_id)
+      self.set_aktif_mi(aktif_mi)
+
+   def get_wallet_id(self) -> str:
+      return self.__wallet_id
+
+   def set_wallet_id(self, wallet_id: str) -> None:
+      if not isinstance(wallet_id, str):
+         raise ValueError("wallet_id metin olmalıdır.")
+      wallet_id = wallet_id.strip()
+
+      if wallet_id == "":
+         raise ValueError("wallet_id boş olamaz.")
+      if len(wallet_id) < 6:
+         raise ValueError("wallet_id en az 6 karakter olmalıdır.")
+      if " " in wallet_id:
+         raise ValueError("wallet_id boşluk içeremez.")
+
+      self.__wallet_id = wallet_id
+
+   def masked_wallet_id(self) -> str:
+      if len(self.__wallet_id) <= 4:
+         return "****"
+      return "*" * (len(self.__wallet_id) - 4) + self.__wallet_id[-4:]
+
+    # --- AKTIF MI ---
+   def get_aktif_mi(self) -> bool:
+      return self.__aktif_mi
+
+   def set_aktif_mi(self, aktif_mi: bool) -> None:
+      if not isinstance(aktif_mi, bool):
+         raise ValueError("aktif_mi True veya False olmalıdır.")
+      self.__aktif_mi = aktif_mi
+
+    # --- PAYMENT LOGIC ---
+   def authorize(self, amount: float) -> bool:
+        # 0) Cüzdan aktif mi?
+      if not self.__aktif_mi:
+         return False
+
+        # 1) Tutar geçerli mi?
+      if not self.tutar_gecerli_mi(amount):
+         return False
+
+        # 2) Bakiye var mı?
+      bakiye = self.get_balance()
+      if bakiye is None:
+         return False
+
+        # 3) Bakiye yetiyor mu?
+      if bakiye >= amount:
+         return True
+
+      return False
+
+   def odeme_yap(self, amount: float) -> bool:
+      if not self.authorize(amount):
+         return False
+
+      yeni_bakiye = self.get_balance() - amount
+      self.set_balance(yeni_bakiye)
+      return True
+
+   def iade_yap(self, amount: float) -> bool:
+      if not self.tutar_gecerli_mi(amount):
+         return False
+
+      yeni_bakiye = (self.get_balance() or 0.0) + amount
+      self.set_balance(yeni_bakiye)
+      return True
+
+   @staticmethod
+   def wallet_id_gecerli_mi(wallet_id: str) -> bool:
+      if not isinstance(wallet_id, str):
+         return False
+      
+      wallet_id = wallet_id.strip()
+      if wallet_id == "" or " " in wallet_id:
+         return False
+      return len(wallet_id) >= 6
+
+   @classmethod
+   def odeme_tipi(cls) -> str:
+      return "Online Cüzdan"
+
+   def get_info(self) -> str:
+      durum = "Aktif" if self.__aktif_mi else "Pasif"
+      return (
+            f"{self.__class__.__name__} | Kişi: {self.get_owner()} | "
+            f"Cüzdan: {self.masked_wallet_id()} | Durum: {durum} | "
+            f"Para Birimi: {self.get_currency()} | Bakiye: {self.kullanilabilir_tutar():.2f}"
+        )
+
+
+
+
+
+ 
+
+
+   
+
+   
+         
+"""
 class OdemeYonetimi:
     def __init__(self):
         self.islem_gecmisi = []  # işlem kayıtlarını tutar
@@ -175,7 +341,7 @@ class OdemeYonetimi:
     def islem_gecmisini_listele(self):
         return self.islem_gecmisi
     
-
+"""
 class YemekhaneServisi:
    pass
 
